7f4f7f3bf5636cb625c1a71c5e67c0f906bc40cb
MaximKursakov <maximcarlos@gmx.de>
2023-01-11 21:26:43 +0100

    test

--- src/components/AspectScore/Aspect-rating/Aspect-rating.js
+++ src/components/AspectScore/Aspect-rating/Aspect-rating.js
@@ -13,6 +13,7 @@ export function AspectRating() {
                 </svg>
                 <p>{scoreNumber}%</p>
             </div>
+            <button>quality</button>
         </section>
     )
 }
